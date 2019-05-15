ba7d1655d6c686b54b8befcb7bfe02431725c12c
Timmy <timgabrielnguyen@gmail.com>
2019-05-14 22:57:56 -0700

    Sorted Array Square

--- array-square.js
+++ array-square.js
@@ -4,6 +4,6 @@ const square = x => {
   return x * x;
 };
 
-const squaredArray = array.map(number => square(number));
+const squaredArray = array.map(number => square(number)).sort((a, b) => a - b);
 
 console.log(squaredArray);
